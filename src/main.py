73b870dda3d16e2b42a51384daf66a67b2fcb7ea
yude <i@yude.jp>
2022-03-17 15:33:11 +0900

    Cleanup

--- src/main.py
+++ src/main.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 
-# pip modules
-from requests import NullHandler
-
 # functions
 from dice import simple_dice
 from uptime import get_uptime
 from git_hash import get_hash
 from wol import issue_wol
-from stream import TweetStream, StreamListener, get_stream
+from stream import get_stream
 
 if __name__ == "__main__":
     get_stream().start()
