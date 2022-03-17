179478cc446774ad8a7778c532326b9300bb437a
yude <i@yude.jp>
2022-03-17 15:34:00 +0900

    Cleanup ②

--- src/main.py
+++ src/main.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# functions
-from dice import simple_dice
-from uptime import get_uptime
-from git_hash import get_hash
-from wol import issue_wol
 from stream import get_stream
 
 if __name__ == "__main__":
